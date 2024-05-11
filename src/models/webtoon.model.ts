e17619b2b9435709274c4815bdde42a9e0073286
hyeri-woo <woohyeri0525@gmail.com>
2024-05-11 23:47:42 +0900

    [feat] update webtoon model

--- src/models/webtoon.model.ts
+++ src/models/webtoon.model.ts
@@ -1,9 +1,10 @@
 import { Schema, model, InferSchemaType } from "mongoose";
+import { SerialCycleType, SerialDateType } from "../types/webtoon";
 
 const webtoonSchema = new Schema({
   //   _id: { type: String, required: true, unique: true, trim: true },
   title: { type: String, required: true, trim: false },
-  author: { type: String, required: true, trim: false },
+  author: { type: [String], required: true, trim: false },
   platform: [String],
   keyword: [String],
   description: { type: String, required: true, trim: false },
@@ -16,6 +17,9 @@ const webtoonSchema = new Schema({
   original_author: { type: String, required: false, trim: false },
   is_free: { type: Boolean, required: true, default: false },
   r_rated: { type: Boolean, required: true, default: false },
+  thumbnail: { type: String },
+  serial_date: SerialDateType,
+  serialCycle: SerialCycleType,
 });
 
 export type WebtoonType = InferSchemaType<typeof webtoonSchema>;
