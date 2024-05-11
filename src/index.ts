11eccb3d14d2aa70d8845d68bb8a93acff3aeb7b
hyeri-woo <woohyeri0525@gmail.com>
2024-05-11 16:11:03 +0900

    [rename] change folder name of controller

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import db from "./server/db";
-import webtoonRouter from "./controller/webtoon.controller";
+import webtoonRouter from "./controllers/webtoon.controller";
 
 dotenv.config();
 
